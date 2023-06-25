894e26e445d84fc4b0eec6a55d129bfea59127c6
Waldisson Bittencourt <waldisson@hotmail.com>
2023-06-24 21:42:29 -0300

    feat: icones, loagind e hook

--- App.tsx
+++ App.tsx
@@ -5,24 +5,31 @@ import {Button} from './src/components/Button/Button';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Text} from './src/components/Text/Text';
+import {Icon} from './src/components/Icon/icon';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text preset="headingLarge" italic>
-            Compoenente Button
+          {/* <Text preset="headingLarge" italic>
+            Componentes
+          </Text>
+          <Button title="Login" />
+          <Icon name="eyeOn" color="carrotSecondary" size={40} /> */}
+
+          <Text preset="headingLarge" marginBottom="s8">
+            Olá!
+          </Text>
+          <Text preset="paragraphLarge" mb="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <Text color="primary" preset="paragraphSmall" bold mt="s10">
+            Esqueci minha senha
           </Text>
 
-          <Button title="Entrar" marginBottom="s12" marginTop="s14" />
-
-          <Button loading title="Entrar" marginBottom="s12" disabled />
-
-          <Button loading title="Loading" marginBottom="s12" />
-
-          <Button preset="outline" title="Outline" marginBottom="s12" />
-          <Button preset="outline" disabled title="Outline" />
+          <Button marginTop="s48" title="Entrar" />
+          <Button preset="outline" marginTop="s12" title="Criar uma conta" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
--- src/components/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -1,15 +1,15 @@
-import {useTheme} from '@shopify/restyle';
 import {
   ActivityIndicator as RNActivityIndicator,
   ActivityIndicatorProps,
 } from 'react-native';
-import {Theme, ThemeColors} from '../../theme/theme';
+import {useAppTheme} from '../../hooks/useAppTheme';
+import {ThemeColors} from '../../theme/theme';
 
 interface Props extends Omit<ActivityIndicatorProps, 'color'> {
   color: ThemeColors;
 }
 
 export function ActivityIndicator({color}: Props) {
-  const {colors} = useTheme<Theme>();
+  const {colors} = useAppTheme();
   return <RNActivityIndicator color={colors[color]} />;
 }
