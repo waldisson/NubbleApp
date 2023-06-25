dc3e69ba0167c00d71810ee0c45eedde129e56ba
Waldisson Bittencourt <waldisson@hotmail.com>
2023-06-25 08:20:58 -0300

    feat: criado o  campo de click em volta do input

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {
+  Pressable,
   TextInput as RNTextInput,
   TextInputProps as RNTextInputProps,
   TextStyle,
@@ -14,24 +15,32 @@ interface TextInputProps extends RNTextInputProps {
 }
 export function TextInput({label, ...rnTextInputProps}: TextInputProps) {
   const {colors} = useAppTheme();
+  const inputRef = useRef<RNTextInput>(null);
+
+  function focusInput() {
+    inputRef.current?.focus();
+  }
+
   return (
-    <Box>
-      <Text preset="paragraphMedium" marginBottom="s4">
-        {label}
-      </Text>
-      <Box {...$textInputContainer}>
-        <RNTextInput
-          placeholderTextColor={colors.gray2}
-          style={$textInputStyle}
-          {...rnTextInputProps}
-        />
+    <Pressable onPress={focusInput}>
+      <Box>
+        <Text preset="paragraphMedium" marginBottom="s4">
+          {label}
+        </Text>
+        <Box {...$textInputContainer}>
+          <RNTextInput
+            ref={inputRef}
+            placeholderTextColor={colors.gray2}
+            style={$textInputStyle}
+            {...rnTextInputProps}
+          />
+        </Box>
       </Box>
-    </Box>
+    </Pressable>
   );
 }
 
 const $textInputStyle: TextStyle = {
-  borderWidth: 1,
   fontFamily: $fontFamily.regular,
   ...$fontSizes.paragraphMedium,
 };
