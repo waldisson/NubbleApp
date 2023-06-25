a2e02cac3c63f12394b32347d353b6a7b5ea3252
Waldisson Bittencourt <waldisson@hotmail.com>
2023-06-25 08:16:05 -0300

    feat: exportando o fontsize e o fantfamily e também recebendo no componente de input

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import {
   TextInput as RNTextInput,
   TextInputProps as RNTextInputProps,
+  TextStyle,
 } from 'react-native';
+import {useAppTheme} from '../../hooks/useAppTheme';
+
 import {Box, BoxProps} from '../Box/Box';
-import {Text} from '../Text/Text';
+import {$fontFamily, $fontSizes, Text} from '../Text/Text';
 
 interface TextInputProps extends RNTextInputProps {
   label: string;
 }
 export function TextInput({label, ...rnTextInputProps}: TextInputProps) {
+  const {colors} = useAppTheme();
   return (
     <Box>
       <Text preset="paragraphMedium" marginBottom="s4">
@@ -17,13 +21,20 @@ export function TextInput({label, ...rnTextInputProps}: TextInputProps) {
       </Text>
       <Box {...$textInputContainer}>
         <RNTextInput
-          style={{borderWidth: 1, height: 50}}
+          placeholderTextColor={colors.gray2}
+          style={$textInputStyle}
           {...rnTextInputProps}
         />
       </Box>
     </Box>
   );
 }
+
+const $textInputStyle: TextStyle = {
+  borderWidth: 1,
+  fontFamily: $fontFamily.regular,
+  ...$fontSizes.paragraphMedium,
+};
 
 const $textInputContainer: BoxProps = {
   borderWidth: 1,
