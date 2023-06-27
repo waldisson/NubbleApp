a5835337bc9b3338deb203636bf5689a144752ad
Waldisson Bittencourt <waldisson@hotmail.com>
2023-06-26 22:07:15 -0300

    feat: tela de navegação criada

--- App.tsx
+++ App.tsx
@@ -3,16 +3,14 @@ import React from 'react';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 
-import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {SignUpScreen} from './src/screens/auth/SignUpScreen/SignUpScreen';
+import {Router} from './src/routes/Routes';
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        {/* <LoginScreen /> */}
-        <SignUpScreen />
+        <Router />
       </ThemeProvider>
     </SafeAreaProvider>
   );
