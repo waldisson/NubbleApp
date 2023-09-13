063594e3f70719b5bd45c2c2ee20d7263ebd9919
Waldisson Bittencourt <waldisson@hotmail.com>
2023-09-13 19:03:18 +0100

    feat: flatlist e api

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-import {Button, Screen, Text} from '@components';
+import {Dimensions, FlatList, Image, ListRenderItemInfo} from 'react-native';
+import {Post, postService} from '@domain';
+
+import {Box, Screen, Text} from '@components';
+
 import {AppTabScreenProps} from '@routes';
 
 export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
+  const [postList, setPostList] = useState<Post[]>([]);
+
+  useEffect(() => {
+    postService.getList().then(list => setPostList(list));
+  }, []);
+
+  function renderItem({item}: ListRenderItemInfo<Post>) {
+    return (
+      <Box>
+        <Box flexDirection="row">
+          <Image
+            source={{uri: item.author.profileURL}}
+            style={{width: 32, height: 32}}
+          />
+          <Text>{item.author.userName}</Text>
+        </Box>
+        <Image
+          source={{uri: item.imageURL}}
+          resizeMode="cover"
+          style={{width: Dimensions.get('screen').width, height: 300}}
+        />
+      </Box>
+    );
+  }
+
   return (
     <Screen>
-      <Text preset="headingMedium">Home Screen</Text>
-      <Button
-        title="Settings"
-        onPress={() => navigation.navigate('SettingsScreen')}
+      <FlatList
+        data={postList}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
       />
     </Screen>
   );
