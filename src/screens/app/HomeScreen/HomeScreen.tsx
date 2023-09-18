eaa33f8407ff4aa3840e32a71c196f4ca759333e
Waldisson Bittencourt <waldisson@hotmail.com>
2023-09-18 19:57:08 +0100

    feat: home header create

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -6,6 +6,8 @@ import {Post, postService} from '@domain';
 import {PostItem, Screen} from '@components';
 import {AppTabScreenProps} from '@routes';
 
+import {HomeHeader} from './components/HomeHeader';
+
 export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
   const [postList, setPostList] = useState<Post[]>([]);
 
@@ -23,6 +25,7 @@ export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
         data={postList}
         keyExtractor={item => item.id}
         renderItem={renderItem}
+        ListHeaderComponent={<HomeHeader />}
         showsVerticalScrollIndicator={false}
       />
     </Screen>
