ac36146cf0bde3dbd0401b902fe98504548c675d
Waldisson Bittencourt <waldisson@hotmail.com>
2023-10-15 17:12:44 +0100

    feat: criação do empty da home

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://10.0.2.2:3333/',
+  baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer MTM.kgzIOyxHvaU1fh7vo9Pn3pO55M_zkjgc8IfcZM6aJP_bYgH7A8QBlZLDTBWw',
+      'Bearer MTQ.aa5F6th20s4_1IGQH_r1cbhmcqarSlBof98HUJkyopfNZ0XnQJAzJsX-j3QH',
   },
 });
--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -6,18 +6,36 @@ import {Post, postService} from '@domain';
 import {PostItem, Screen} from '@components';
 import {AppTabScreenProps} from '@routes';
 
+import {HomeEmpty} from './components/HomeEmpty';
 import {HomeHeader} from './components/HomeHeader';
 
-export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
+export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Boolean | null>(null);
   const [postList, setPostList] = useState<Post[]>([]);
 
+  async function fechData() {
+    try {
+      setError(null);
+      setLoading(true);
+      const list = await postService.getList();
+      setPostList(list);
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   useEffect(() => {
-    postService.getList().then(list => setPostList(list));
+    fechData();
   }, []);
 
   function renderItem({item}: ListRenderItemInfo<Post>) {
     return <PostItem post={item} />;
   }
+  console.log(postList);
 
   return (
     <Screen style={$screen}>
@@ -25,8 +43,12 @@ export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
         data={postList}
         keyExtractor={item => item.id}
         renderItem={renderItem}
+        contentContainerStyle={{flex: postList.length === 0 ? 1 : 0}}
         ListHeaderComponent={<HomeHeader />}
         showsVerticalScrollIndicator={false}
+        ListEmptyComponent={
+          <HomeEmpty refetch={fechData} loading={loading} error={error} />
+        }
       />
     </Screen>
   );
@@ -36,4 +58,5 @@ const $screen: StyleProp<ViewStyle> = {
   paddingBottom: 0,
   paddingHorizontal: 0,
   paddingTop: 0,
+  flex: 1,
 };
