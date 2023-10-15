03f36f85729329357bcddd3b8d14a008a1e6513a
Waldisson Bittencourt <waldisson@hotmail.com>
2023-10-15 17:15:22 +0100

    chore: base url altearda para acesso

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333/',
+  baseURL: 'http://10.0.2.2:3333/',
   headers: {
     Authorization:
       'Bearer MTQ.aa5F6th20s4_1IGQH_r1cbhmcqarSlBof98HUJkyopfNZ0XnQJAzJsX-j3QH',
